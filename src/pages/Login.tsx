3d093f932648d449eb081cd80c8775597f7b5acf
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-15 14:13:45 +0530

    Implement Login page and handle userLogin functionality

---
+++ src/pages/Login.tsx
@@ -0,0 +1,102 @@
+import { TextField, Button, Box, Typography, Paper } from "@mui/material";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit: userLogin,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const navigate = useNavigate();
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/user?email=${encodeURIComponent(
+          data.email
+        )}&password=${encodeURIComponent(data.password)}`
+      );
+
+      if (response.status === 200) {
+        window.alert("Logged in");
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 404) {
+        setError("email", {
+          type: "manual",
+          message: "Email is not registered",
+        });
+      } else if (error.response && error.response.status === 401) {
+        setError("password", { type: "manual", message: "Incorrect password" });
+      } else {
+        console.error("Error while logging in", error);
+      }
+    }
+  };
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        padding: "2rem",
+        maxWidth: "400px",
+        margin: "5rem auto",
+        fontFamily: "serif",
+      }}
+    >
+      <Typography
+        variant="h5"
+        gutterBottom
+        sx={{ fontFamily: "serif", textAlign: "center" }}
+      >
+        Login
+      </Typography>
+      <form onSubmit={userLogin(onSubmit)}>
+        <Box display="flex" flexDirection="column" gap={2}>
+          <TextField
+            label="Email"
+            type="email"
+            {...register("email", { required: "Email is required" })}
+            fullWidth
+            error={!!errors.email}
+            helperText={errors.email ? (errors.email.message as string) : ""}
+          />
+
+          <TextField
+            label="Password"
+            type="password"
+            {...register("password", { required: "Password is required" })}
+            fullWidth
+            error={!!errors.password}
+            helperText={
+              errors.password ? (errors.password.message as string) : ""
+            }
+          />
+          <Button type="submit" variant="contained" color="secondary" fullWidth>
+            Login
+          </Button>
+        </Box>
+
+        <p style={{ textAlign: "center", fontSize: 16 }}>
+          Don't have an account?{" "}
+          <button
+            style={{
+              background: "white",
+              color: "purple",
+              border: 0,
+              fontSize: 16,
+            }}
+            onClick={() => navigate("/register")}
+          >
+            Register here
+          </button>
+        </p>
+      </form>
+    </Paper>
+  );
+};
+
+export default LoginForm;
