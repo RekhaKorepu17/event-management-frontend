beaa1340f151f8816e648b118a2800041eb24468
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-17 12:23:27 +0530

    Update App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import RegisterForm from './pages/authentication/Register';
-import LoginForm from './pages/authentication/Login';
-import DashBoard from './pages/dashboard/DashBoard';
-import { StateProvider } from './GlobalContext/globalContext';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import RegisterForm from "./pages/authentication/Register";
+import LoginForm from "./pages/authentication/Login";
+import DashBoard from "./pages/dashboard/DashBoard";
+import { StateProvider } from "./GlobalContext/globalContext";
+import Event from "./pages/event-creation/Event";
+import EventDetails from "./pages/event-details/EventDetails";
 
 function App() {
   return (
     <StateProvider>
       <Router>
         <Routes>
-          <Route path="/dashboard" element={<DashBoard />} />
           <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/EventDetails" element={<EventDetails />} />
         </Routes>
       </Router>
     </StateProvider>
   );
 }
-
 
 export default App;
